b24e2c1b43756083b155882299fb4fd5520dbba9
rmasianjr <ricardo.masianjr@gmail.com>
2025-04-29 08:58:19 +0800

    Add styling to create page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -31,58 +31,101 @@ export default function CreatePage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
+    <form
+      className="max-w-sm md:max-w-2/6 mx-auto p-5 my-5 bg-white border border-gray-200 rounded-lg shadow-md"
+      onSubmit={handleSubmit}
+    >
+      <div className="mb-5">
+        <label
+          htmlFor="name"
+          className="text-sm font-medium text-gray-900 block mb-2"
+        >
+          Name
+        </label>
         <input
+          className="text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg block w-full p-2.5 outline-none focus:border-blue-500"
           type="text"
           id="name"
           name="name"
           value={formData.name}
           onChange={handleChange}
+          required
         />
       </div>
-      <div>
-        <label htmlFor="age">Age:</label>
+      <div className="mb-5">
+        <label
+          htmlFor="age"
+          className="text-sm font-medium text-gray-900 block mb-2"
+        >
+          Age
+        </label>
         <input
+          className="text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg block w-full p-2.5 outline-none focus:border-blue-500"
           type="number"
           id="age"
           name="age"
           value={formData.age}
           onChange={handleChange}
+          required
         />
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="text-sm font-medium text-gray-900 block mb-2"
+        >
+          Email
+        </label>
         <input
+          className="text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg block w-full p-2.5 outline-none focus:border-blue-500"
           type="email"
           id="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
+          required
         />
       </div>
-      <div>
-        <label htmlFor="contact">Contact Number:</label>
+      <div className="mb-5">
+        <label
+          htmlFor="contact"
+          className="text-sm font-medium text-gray-900 block mb-2"
+        >
+          Contact Number
+        </label>
         <input
+          className="text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg block w-full p-2.5 outline-none focus:border-blue-500"
           type="tel"
           id="contact"
           name="contact"
           value={formData.contact}
           onChange={handleChange}
+          required
         />
       </div>
-      <div>
-        <label htmlFor="address">Address:</label>
+      <div className="mb-5">
+        <label
+          htmlFor="address"
+          className="text-sm font-medium text-gray-900 block mb-2"
+        >
+          Address
+        </label>
         <input
+          className="text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg block w-full p-2.5 outline-none focus:border-blue-500"
           type="text"
           id="address"
           name="address"
           value={formData.address}
           onChange={handleChange}
+          required
         />
       </div>
-      <button type="submit">Submit</button>
+      <button
+        className="text-center text-sm font-medium text-white bg-blue-700 px-5 py-2.5 rounded-lg focus:outline-blue-300 hover:bg-blue-800"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
   );
 }
